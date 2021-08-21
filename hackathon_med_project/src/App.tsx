ab3fb3c667650d22b21e4f451440be63f37dccfe
niko verrios <nver0008@student.monash.edu>
2021-08-21 14:58:54 +1000

    new

--- hackathon_med_project/src/App.tsx
+++ hackathon_med_project/src/App.tsx
@@ -4,9 +4,7 @@ import * as style from "./App.style";
 const App: React.FC = () => {
   return (
     <div>
-      <h1 className={style.title}>
-        Welcome to our Hackathon Medical Information
-      </h1>
+      <h1 className={style.title}>Welcome to our Hackathon Medi-</h1>
     </div>
   );
 };
