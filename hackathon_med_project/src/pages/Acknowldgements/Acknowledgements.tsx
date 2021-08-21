8e8a4c3ce417090b8e3879042658dafdb1b5e6e9
Tim <tjor0005@student.monash.edu>
2021-08-22 02:58:22 +1000

    Added links for acknowledgements

--- hackathon_med_project/src/pages/Acknowldgements/Acknowledgements.tsx
+++ hackathon_med_project/src/pages/Acknowldgements/Acknowledgements.tsx
@@ -5,14 +5,36 @@ import { body as appStyle } from "../../App.style";
 const Acknowledgements: React.FC = () => {
   return (
     <div className={appStyle}>
-      <h1 className={style.title}>Aknowledgements</h1>
+      <h1 className={style.title}>Acknowledgements</h1>
       <h3>
         We would like to aknowledge these websites where we have got our
         information from.
       </h3>
       <div>
         <p>https://localhost:3000</p>
-        <p>Insert links here</p>
+        <a href="https://www.heartfoundation.org.au/bmi-calculator">
+          https://www.heartfoundation.org.au/bmi-calculator
+        </a>
+        <p></p>
+        <a href="https://www.medicinenet.com/weight_gain_shockers_pictures_slideshow/article.htm">
+          https://www.medicinenet.com/weight_gain_shockers_pictures_slideshow/article.htm
+        </a>
+        <p></p>
+        <a href="https://www.beyondblue.org.au/">
+          https://www.beyondblue.org.au/
+        </a>
+        <p></p>
+        <a href="https://www.calculator.net/bmr-calculator.html?ctype=metric&cage=25&csex=m&cheightfeet=5&cheightinch=10&cpound=160&cheightmeter=180&ckg=70&cmop=1&coutunit=j&cformula=m&cfatpct=20&x=81&y=11">
+          https://www.calculator.net/bmr-calculator.html?ctype=metric&cage=25&csex=m&cheightfeet=5&cheightinch=10&cpound=160&cheightmeter=180&ckg=70&cmop=1&coutunit=j&cformula=m&cfatpct=20&x=81&y=11
+        </a>
+        <p></p>
+        <a href="https://academic.oup.com/ajcn">
+          https://academic.oup.com/ajcn
+        </a>
+        <p></p>
+        <a href="https://inbodyusa.com/blogs/inbodyblog/49311425-how-to-use-bmr-to-hack-your-diet/">
+          https://inbodyusa.com/blogs/inbodyblog/49311425-how-to-use-bmr-to-hack-your-diet/
+        </a>
       </div>
       <hr color="black"></hr>
       <div>
