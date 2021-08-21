09506655f47c3630d89a4fd8cb7a1a8d3f7c634b
timnumber1 <tim.bard.han@gmail.com>
2021-08-21 19:11:42 +1000

    Trying out Reactjs with the calculator

--- hackathon_med_project/src/pages/BMI/BMI.style.tsx
+++ hackathon_med_project/src/pages/BMI/BMI.style.tsx
@@ -6,3 +6,20 @@ export const title = s({
   fontSize: px(36),
   marginLeft: px(120),
 });
+
+export const marginalise_left = s({
+  textAlign: "left",
+  marginLeft: px(24),
+});
+
+export const bmi_results = s({
+  textAlign: "center",
+  marginTop: px(10),
+});
+
+export const col = s({
+  columnCount: 2,
+  columnFill: "balance",
+  textAlign: "center",
+  marginTop: px(10),
+});
--- hackathon_med_project/src/pages/BMI/BMI.tsx
+++ hackathon_med_project/src/pages/BMI/BMI.tsx
@@ -1,3 +1,4 @@
+import { px } from "csx";
 import React from "react";
 import * as style from "./BMI.style";
 
@@ -5,7 +6,20 @@ const BMI = () => {
   return (
     <div>
       <h1 className={style.title}>BMI- Body Mass Index</h1>
-      <h1> This is the body and because it is a body it is a body </h1>{" "}
+      <p className={style.marginalise_left}>
+        {" "}
+        The Body Mass Index measures the relationship between your height to
+        weight to determine if you are healthy. Use our BMI calculator to see
+        how healthy you are.
+      </p>{" "}
+      <div className={style.col}>
+        <h2> BMI Calculator </h2>
+        <p> This is </p>
+        <h2 className={style.bmi_results}> BMI Results </h2>
+      </div>
+      <form>
+        <label></label>
+      </form>
     </div>
   );
 };
