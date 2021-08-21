8c2174778a88761ce141c82459b555f232bccafe
niko verrios <nver0008@student.monash.edu>
2021-08-21 20:43:38 +1000

    jordan merge

--- hackathon_med_project/src/pages/BMR/BMR.style.tsx
+++ hackathon_med_project/src/pages/BMR/BMR.style.tsx
@@ -6,3 +6,12 @@ export const title = s({
   fontSize: px(24),
   marginLeft: px(120),
 });
+
+export const column = s({
+  columns: 2,
+  columnWidth: 10,
+});
+
+export const center = s({
+  textAlign: "center",
+});
