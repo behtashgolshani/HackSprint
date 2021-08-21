d1b99bc27ddbc7c05918f762cd75d153aba4ef0f
niko verrios <nver0008@student.monash.edu>
2021-08-21 15:41:37 +1000

    add routing

--- hackathon_med_project/src/App.tsx
+++ hackathon_med_project/src/App.tsx
@@ -1,11 +1,42 @@
 import React from "react";
 import * as style from "./App.style";
+import { HeaderBar } from "./features/HeaderBar";
+import { Home } from "./pages/Home";
+import {
+  BrowserRouter as Router,
+  Route,
+  RouteComponentProps,
+  Switch,
+} from "react-router-dom";
+import routes from "./config/routes";
 
 const App: React.FC = () => {
   return (
-    <div>
-      <h1 className={style.title}>Welcome to our Hackathon Medi-</h1>
-    </div>
+    <Router>
+      <div className={style.app}>
+        <HeaderBar />
+        <div className={style.content}>
+          <Switch>
+            {Object.values(routes).map((route, index) => {
+              return (
+                <Route
+                  key={index}
+                  path={route.path}
+                  exact={route.exact}
+                  render={(props: RouteComponentProps) => (
+                    <route.component
+                      name={route.name}
+                      {...props}
+                      {...route.props}
+                    />
+                  )}
+                />
+              );
+            })}
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 };
 
