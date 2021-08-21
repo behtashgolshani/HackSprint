b76ee1d366a9232b43d4f19abe95219854147316
niko verrios <nver0008@student.monash.edu>
2021-08-21 23:07:40 +1000

    before rebase

---
+++ hackathon_med_project/src/pages/Acknowldgements/Acknowledgements.style.tsx
@@ -0,0 +1,16 @@
+import { style as s } from "typestyle";
+import { percent, px } from "csx";
+
+export const title = s({
+  textAlign: "left",
+});
+
+export const content = s({
+  paddingLeft: percent(5),
+  paddingTop: px(20),
+  width: percent(90),
+});
+
+export const references = {
+  fontSize: px(20),
+};
